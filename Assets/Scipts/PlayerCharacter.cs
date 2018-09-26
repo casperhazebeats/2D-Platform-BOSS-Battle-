33fcaec51c0fd39df6cfe58f6f4917e5f298e427
unknown <Jason.Fishel@iam.local>
2018-09-26 15:20:10 -0500

    bunch of coding
    edit code so you can run!

--- Assets/Scipts/PlayerCharacter.cs
+++ Assets/Scipts/PlayerCharacter.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
+    [SerializeField]
+    private int lives = 3; // PascalCase or camel Case
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private string name = "Mario";
+
+    [SerializeField]
+    private float jumpHeight = 5, speed = 5;
+
+    private bool hasKey;
+
+    private bool isOnGround;
+    // Use this for initialization
+    void Start () {
         Debug.Log("hello");
+
+        string pizza = "yum";
+        Debug.Log(pizza);
+
+        transform.Translate(0, -.01f, 0);
+        if (hasKey)
+        {
+            float openTimer = 5;
+            Debug.Log("Open the door!");
+            for (int i = 0; i < openTimer; i++)
+            {
+            }
+
+            rigidbody2DInstance.gravityScale = 5;
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update ()
+         (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+       MoveLeft();
 	}
+    private void Move()
+    {
+
+    }
 }
