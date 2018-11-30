6bc2a172c607bc6178deae35e1baf94acf1c5ee5
spreadjelly7 <jason.fishel@loop.colum.edu>
2018-11-30 02:08:52 -0600

    Animated Collectables done. Test Level additions in description.
    -special feature
    -Add cinemachine
    -score on screen (rollaball)
    -textures
    -animations (idle move jump die)
    -title screen and credits info
    -title screen music (maybe use the continuous)
    -death sound effect

---
+++ Assets/Scipts/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour 
+{
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2;
+    private static int coinCount;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2 = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            coinCount++;
+            Debug.Log("Coin count: " + coinCount);
+            audioSource.Play();
+            spriteRenderer.enabled = false;
+            boxCollider2.enabled = false;
+            Destroy(this.gameObject, audioSource.clip.length);
+        }
+    }
+}
