76a04c51c28fdbe36ed158047eb867b5172d6961
spreadjelly7 <jason.fishel@loop.colum.edu>
2018-12-02 02:52:10 -0600

    idle, move, and jump animations added
    -special feature
    -background (cinemachine polygon)
    -get die(explosion) animation working
    -textures
    -title screen and credits info
    -title screen music (maybe use the continuous)
    -death sound effect

--- Assets/Scipts/Hazard.cs
+++ Assets/Scipts/Hazard.cs
@@ -1,23 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Hazard : MonoBehaviour
+public class Hazard : MonoBehaviour{
+private Animator anim;
+
+    void Start()
 {
+    anim = GetComponent<Animator>();
+}
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player entered hazard.");
             PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            //collision.transform.parent.GetComponent<PlayerAnim>();
             player.Respawn();
         }
         else
         {
             Debug.Log("Something other than Player entered the Hazard!");
         }
-      
+
+}
+
+    private void Hit()
+    {
+        anim.SetTrigger("Die");
+        Invoke("Explode", 0.01f);
+
     }
-  
+
 }
---
+++ Assets/Scipts/PlayerAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour {
+
+    private Animator anim;
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetKey(KeyCode.LeftArrow)|| Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("IsSkipping", true);
+        }
+        else
+        {
+            anim.SetBool("IsSkipping", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("IsJumping");
+        }
+	}
+}
