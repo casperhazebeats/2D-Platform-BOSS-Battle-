9c9ccc82cc0d3557e37ae947ebc8845b3b0f736c
spreadjelly7 <jason.fishel@loop.colum.edu>
2018-12-04 03:45:35 -0600

    Menu Scene Finished (no polish)
    -special feature (hinge joint)
    -background (cinemachine polygon)
    -textures

---
+++ Assets/Unity UI Samples/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+    public AudioSource myFX;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void HoverSound()
+    {
+        myFX.PlayOneShot(hoverFX);
+    }
+
+    public void ClickSound()
+    {
+        myFX.PlayOneShot(clickFX);
+    }
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Assets/Unity UI Samples/Scripts/SelectWithKeyboardorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectWithKeyboardorController : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    public float EPSILON { get; private set; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (System.Math.Abs(Input.GetAxisRaw("Vertical")) > EPSILON && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+	}
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
